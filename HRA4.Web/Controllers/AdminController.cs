c940e1e492f3aa641fb37ecea7e58eb1bff7de2c
MahitoshK <mahitosh.kumar@Silicus.com>
2015-12-18 16:56:07 +0530

    controllers

---
+++ HRA4.Web/Controllers/AdminController.cs
@@ -0,0 +1,44 @@
+﻿using HRA4.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HRA4.Web.Controllers
+{
+    public class AdminController : BaseController
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Institution(string tenant)
+        {
+            return View();
+        }
+
+        public ActionResult Dashboard()
+        {
+            var instituionList = _applicationContext.ServiceContext.AdminService.GetTenants();
+            return View(instituionList);
+        }
+
+        public ActionResult AddInstitution()
+        {
+            return View();
+        }
+
+         [HttpPost]         
+        public ActionResult Create(Institution instituion)
+        {
+            var institution = _applicationContext.ServiceContext.AdminService.AddUpdateTenant(instituion);
+            Task taskA = Task.Run(() => _applicationContext.ServiceContext.AdminService.CreateTenantDb(institution));
+            return RedirectToAction("Dashboard");
+        }
+    }
+}
---
+++ HRA4.Web/Controllers/BaseController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HRA4.Context;
+namespace HRA4.Web.Controllers
+{
+    public class BaseController : Controller
+    {
+        public IApplicationContext _applicationContext;
+
+
+        public BaseController()
+        {
+            _applicationContext = new ApplicationContext();
+            //var temp =_applicationContext.Services.AppointmentService.GetAppointments();
+            // var temp = ApplicationContext.AppointmentService.GetAppointments();
+        }
+    }
+}
