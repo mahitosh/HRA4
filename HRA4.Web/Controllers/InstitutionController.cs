ab231e2efad9db72fbc822aa86dec269e062a105
MahitoshK <mahitosh.kumar@Silicus.com>
2015-12-28 12:17:52 +0530

    Changes in AppointmentList

--- HRA4.Web/Controllers/InstitutionController.cs
+++ HRA4.Web/Controllers/InstitutionController.cs
@@ -19,41 +19,48 @@ namespace HRA4.Web.Controllers
             List<ViewModels.Appointment> apps = new List<ViewModels.Appointment>();
             var instList = _applicationContext.ServiceContext.AdminService.GetTenants();
             ViewBag.instListcount = instList.Count;
-            if(Id!= null && Id>0)
+            if (instList.Count == 0)
             {
-            Session.Add("InstitutionId", Id);
-                int v2 = Id ?? default(int);
-                //_applicationContext = new ApplicationContext();
-                 apps = _applicationContext.ServiceContext.AppointmentService.GetAppointments(v2);
                 return View(apps);
             }
-            return View(apps);
-            //return RedirectToAction("ManageInstitution","Admin");
+            if (Id != null && Id > 0)
+            {
+                Session.Add("InstitutionId", Id);
+                int v2 = Id ?? default(int);
+                //_applicationContext = new ApplicationContext();
+                apps = _applicationContext.ServiceContext.AppointmentService.GetAppointments(v2);
+                return View(apps);
+            }
+            // return View(apps);
+            return RedirectToAction("ManageInstitution", "Admin");
 
-            
+
         }
-        
-        public JsonResult FilteredInstitution(string name,string dob,string appdt)
+
+        public JsonResult FilteredInstitution(string name, string dob, string appdt)
         {
-             //Session.Add("InstitutionId", 1);
+            //Session.Add("InstitutionId", 1);
+            string view = string.Empty;
+             
+            if (Session != null && Session["InstitutionId"] != null)
+            {
 
-           // _applicationContext = new ApplicationContext();
-            int instId = (int)Session["InstitutionId"];
-            var apps = _applicationContext.ServiceContext.AppointmentService.GetAppointments(instId).Where(a => a.PatientName.Trim().ToLower().Contains(name.Trim().ToLower()) );
+                int instId = (int)Session["InstitutionId"];
+                var apps = _applicationContext.ServiceContext.AppointmentService.GetAppointments(instId).Where(a => a.PatientName.Trim().ToLower().Contains(name.Trim().ToLower()));
 
-            if(dob.Trim().Length > 0)
-            apps = apps.Where(a => a.DateOfBirth.ToString().Trim().Contains(dob.Trim()));
+                if (dob.Trim().Length > 0)
+                    apps = apps.Where(a => a.DateOfBirth.ToString().Trim().Contains(dob.Trim()));
 
-            if (appdt.Trim().Length > 0)
-                apps = apps.Where(a => a.AppointmentDate.ToString().Trim().Contains(appdt.Trim()));
+                if (appdt.Trim().Length > 0)
+                    apps = apps.Where(a => a.AppointmentDate.ToString().Trim().Contains(appdt.Trim()));
 
-            
-            var view = RenderPartialView("_InstitutionGrid", apps);
-
+                view = RenderPartialView("_InstitutionGrid", apps);
+            }
             var result = new { view = view };
 
             return Json(result, JsonRequestBehavior.AllowGet);
-            
+
+
 
         }
 
@@ -61,7 +68,7 @@ namespace HRA4.Web.Controllers
         {
             return View();
         }
-      
+
 
         protected virtual string RenderPartialView(string partialViewName, object model)
         {
