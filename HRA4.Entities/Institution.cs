f83c02f65f001f0b721efd8d42e5b8f7a6ed92be
MahitoshK <mahitosh.kumar@Silicus.com>
2015-12-28 18:23:59 +0530

    Sprint 1 QA Testing

--- HRA4.Entities/Institution.cs
+++ HRA4.Entities/Institution.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Web.Mvc;
-
+using System.ComponentModel.DataAnnotations;
 namespace HRA4.Entities
 {
     public class Institution
@@ -13,6 +13,8 @@ namespace HRA4.Entities
         public string InstitutionName { get; set; }
         public string DbName { get; set; }
         public string RaInstitutionId { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}", ApplyFormatInEditMode = true)]
         public DateTime DateCreated { get; set; }
 
 
