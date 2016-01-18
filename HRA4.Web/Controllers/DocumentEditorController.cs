4a3f3b7f3d60f750a55d906ef02354597c8ad03a
nilesh-sonawane <nilesh.sonawane@silicus.com>
2016-01-18 14:37:22 +0530

    RACPA-16 : New Document Editor . Html Integration and TinyMCE text editor is added at application

---
+++ HRA4.Web/Controllers/DocumentEditorController.cs
@@ -0,0 +1,18 @@
+﻿using HRA4.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HRA4.Web.Controllers
+{
+    public class DocumentEditorController : Controller
+    {
+        // GET: DocumentEditor
+        public ActionResult Index(TinyMCEModelJQuery model )
+        {
+            return View();
+        }
+    }
+}
