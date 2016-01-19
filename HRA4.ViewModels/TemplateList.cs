cfa154a3bc4dfb6af18af6d9712d8354bb87ab1b
nilesh-sonawane <nilesh.sonawane@silicus.com>
2016-01-19 11:04:15 +0530

    RACPA-16 : New Document Editor . View list of Template and based on selection show / Edit Template .

--- HRA4.ViewModels/TemplateList.cs
+++ HRA4.ViewModels/TemplateList.cs
@@ -3,14 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
 
 namespace HRA4.ViewModels
 {
     public class Template
     {
         public int Id { get; set; }
-        public string TemplateName { get; set; }
         public string FinalHtml { get; set; }
+        public string TemplateName { get; set; }
+        /*
+        [AllowHtml]
+        [UIHint("tinymce_jquery_full")]
+        public string Content { get; set; }
+         */ 
     }
     
     public class TemplateList
@@ -19,10 +27,23 @@ namespace HRA4.ViewModels
         {
             SuggestedDocument = new List<Template>();
             OtherDocuments = new List<Template>();
+
         }
+
+        [AllowHtml]
+        [UIHint("tinymce_jquery_full")]
+        public string Content { get; set; }
+
+        public string TemplateName { get; set; }
 
         public List<Template> SuggestedDocument { get; set; }
 
         public List<Template> OtherDocuments { get; set; }
+
+
+
     }
+
+   
+
 }
