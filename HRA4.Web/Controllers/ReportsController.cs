5d5eedb23b566fef007d53357eb260cca3b8c574
adipatil2012 <aditya.patil@silicus.com>
2016-01-19 19:13:26 +0530

    Added controller for Audit Reports

--- HRA4.Web/Controllers/ReportsController.cs
+++ HRA4.Web/Controllers/ReportsController.cs
@@ -3,14 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using HRA4.Context;
+using HRA4.Entities;
 
 namespace HRA4.Web.Controllers
 {
-    public class ReportsController : Controller
+    public class ReportsController : BaseController
     {
         // GET: Reports
         public ActionResult AuditReports()
         {
+            return View();
+        }
+        public ActionResult GetReports(string MRN)
+        {
+            var model=_applicationContext.ServiceContext.AppointmentService.GetAuditReports(MRN,DateTime.Now.ToString(),DateTime.Now.ToString());
+            
             return View();
         }
     }
