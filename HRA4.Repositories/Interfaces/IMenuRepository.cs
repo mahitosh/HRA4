5cece49a82aebef3a8f87714962fddf1b5e2a07c
MahitoshK <mahitosh.kumar@Silicus.com>
2016-02-04 18:21:01 +0530

    MenuRepository

---
+++ HRA4.Repositories/Interfaces/IMenuRepository.cs
@@ -0,0 +1,16 @@
+﻿using HRA4.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRA4.Repositories.Interfaces
+{
+    public interface IMenuRepository
+    {
+        List<Menu> GetMenu();
+        string GetMenuRights(int roleId);
+        string GetExcludeControlIds(int roleId);
+    }
+}
